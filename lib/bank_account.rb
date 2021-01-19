3f8741895596268da69f88cf37b22569e4b3918c
Colin Schlecht <schlechtcolin@yahoo.com>
2021-01-19 00:20:18 -0800

    commit tree 6a18cb180b4b6e9c4374555df91efb9b95ee988f

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
+    attr_accessor :balance, :status
+    attr_reader :name
 
+    @@all = []
+
+    def initialize(name)
+        @balance = 1000
+        @name = name
+        @status = "open"
+    end
+
+    def self.all
+        @@all
+    end
+
+    def deposit(amt)
+        self.balance += amt
+    end
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        self.balance > 0 && self.status == "open"
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,60 @@
+require 'pry'
 class Transfer
-  # your code here
+  
+  attr_accessor :amount, :receiver, :sender, :status
+
+  def initialize(sender, receiver, amount)
+    @amount = amount
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+  end
+
+  def valid?
+    sender.valid?
+    receiver.valid?
+  end
+
+  def accept_transfer
+    self.status = "complete"
+    receiver.deposit(self.amount)
+    sender.deposit(-self.amount)
+  end
+
+  def reject_transfer
+    self.status = "rejected"
+    return "Transaction rejected. Please check your account balance."
+  end
+
+  def execute_transaction
+    if valid? && self.status == "pending" && sender.balance >= self.amount 
+      accept_transfer
+    else
+      reject_transfer
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      receiver.deposit(-self.amount)
+      sender.deposit(self.amount)
+      self.status = "reversed"
+    end
+  end
+
+
+
 end
+
+
+# def execute_transaction
+#   if @status == 'pending' && @sender.balance >= @amount && @sender.valid? && @receiver.valid?
+#     #binding.pry
+#     @receiver.deposit(@amount)
+#     @sender.deposit(-@amount)
+#     @status = 'complete'
+#   else
+#     @status = 'rejected'
+#     "Transaction rejected. Please check your account balance."
+#   end
+# end
